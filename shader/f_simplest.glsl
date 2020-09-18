f152ee287e983ad5cdd7aaa6b08e95b921b2d0cc
Przemek Ambroży <przemek19986@gmail.com>
2020-09-18 15:42:18 +0200

    Nowe shadery, chyba *troszkę* lepsze

--- shader/f_simplest.glsl
+++ shader/f_simplest.glsl
@@ -4,24 +4,30 @@ uniform sampler2D textureMap;
 
 out vec4 pixelColor; //Zmienna wyjsciowa fragment shadera. Zapisuje sie do niej ostateczny (prawie) kolor piksela
 
-in vec4 ic; 
 in vec4 n;
 in vec4 l;
 in vec4 v;
 in vec2 iTexCoord;
 
 void main(void) {
-	
+
 	vec4 ml = normalize(l);
 	vec4 mn = normalize(n);
-	float nl = clamp(dot(mn, ml), 0, 1); //Kosinus kąta pomiędzy wektorami n i l.
-
 	vec4 mv = normalize(v);
-	vec4 mr = reflect(-ml,mn); //Wektor odbity
-	float rv = pow(clamp(dot(mr, mv), 0, 1), 500); // Kosinus kąta pomiędzy wektorami r i v podniesiony do 25 potęgi
+	vec4 mr = reflect(-ml, mn);
+
+	// Kosinus kąta między wektorami n i l
+	float nl = clamp(dot(mn, ml), 0, 1);
+
+	// Kosinus kąta między wektorami r i v podniesiony do potęgi
+	float rv = pow(clamp(dot(mr, mv), 0, 1), 32); 
 
 	vec4 t = texture(textureMap, iTexCoord);
+	//vec4 t = vec4(1.0);
 
-	pixelColor= vec4(t.rgb, t.a) + vec4(rv, rv, rv, 0); //Wyliczenie modelu oświetlenia (bez ambient);
-	//pixelColor = vec4(1,1,1,1);
+	float ambient = 0.35;
+
+	// Wyliczenie koloru piksela
+	pixelColor = vec4(t.rgb * (nl + ambient), t.a) + vec4(rv, rv, rv, 0); 
+
 }
--- shader/v_simplest.glsl
+++ shader/v_simplest.glsl
@@ -12,25 +12,27 @@ in vec4 normal; //wektor normalny w przestrzeni modelu
 in vec2 texCoord;
 
 //Zmienne interpolowane
-out vec4 ic;
 out vec4 l;
 out vec4 n;
 out vec4 v;
 out vec2 iTexCoord;
 
 void main(void) {
-    vec4 lp = vec4(-25, 30, -25, 1);
-    //vec4 lp = vec4(-13.5, 80, -6, 1); //pozcyja światła, przestrzeń świata
 
-    // l = normalize(V * (lp - M * vertex));
-    // v = normalize(vec4(0, 0, 0, 1) - V * M * vertex); //wektor do obserwatora w przestrzeni oka
-    // n = normalize(V * M * normal); //wektor normalny w przestrzeni oka
+    // Pozycja światła w przestrzeni świata
+    //vec4 lp = vec4(20, -120, 50, 1);
+    vec4 lp = vec4(-4, -30, 8, 1);
 
-    l = normalize(V * (lp - M * vertex));//znormalizowany wektor do światła w przestrzeni oka
-    n = normalize(V * M * normal);//znormalizowany wektor normalny w przestrzeni oka
-    v = normalize(vec4(0, 0, 0, 1) - V * M * vertex); //Wektor do obserwatora w przestrzeni oka
+    
+    // Znormalizowany wektor do światła w przestrzeni oka
+    l = normalize(V * (lp - (M * vertex))); 
 
-    ic = vec4(0.5f, 0.8f, 0.5f, 1.0f);
+    // Znormalizowany wektor normalny w przestrzeni oka
+    n = normalize(V * M * normal); 
+
+    // Wektor do obserwatora w przestrzeni oka
+    v = normalize( vec4(0, 0, 0, 1) - V * M * vertex );
+
     iTexCoord = texCoord;
     
     gl_Position=P*V*M*vertex;
